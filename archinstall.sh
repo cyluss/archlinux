f63d4abe98e38224f19cb90c662b20a92162e952
cyluss <41672371+cyluss@users.noreply.github.com>
2023-03-11 02:38:09 +0900

    add archinstall

---
+++ archinstall.sh
@@ -0,0 +1,1 @@
+archinstall --config https://cyluss.github.io/archlinux/user_configuration.json --disk-layout https://cyluss.github.io/archlinux/user_disk_layout.json –cred /root/user_credentials.json
